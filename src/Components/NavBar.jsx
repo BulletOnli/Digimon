5d5e79d66b9e4a41c985d752d0d7ee8584ac0222
Gemmuel <gemmueldelacruz@gmail.com>
2023-02-18 22:36:00 +0800

    back it to normal

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,5 +1,4 @@
 import { NavLink } from "react-router-dom";
-import logo from "/images/logo.png";
 
 const NavBar = (props) => {
     return (
@@ -13,7 +12,7 @@ const NavBar = (props) => {
             <p onClick={props.onclick}>All</p>
             <p onClick={props.onclick}>Fresh</p>
             <p onClick={props.onclick}>In Training</p>
-            <img className="logo" src={logo} alt="" />
+            <img className="logo" src="/images/logo.png" alt="" />
             <p onClick={props.onclick}>Rookie</p>
             <p onClick={props.onclick}>Champion</p>
             <p onClick={props.onclick}>Ultimate</p>
